7e6ef0c19d93b16bb6592c47a8b098d42ee54318
Robbert Brandsma <i@robbertbrandsma.nl>
2015-07-04 12:37:54 +0200

    Include NewRemoteSwitch library

--- PlanB/PlanB.ino
+++ PlanB/PlanB.ino
@@ -27,29 +27,20 @@ using namespace std;
 
 URLHandler handler;
 
-void thingCallback() {
-    Serial.println("CALLBACK OH ME GOD GROUP SWITCH ON EN STUFF OHMEGODGODGOD");
-}
-void otherThingCallBack() {
-    Serial.println("YEAAAAAH OTHER THING CALLBACK AND THIS SHIT WORKS MAN, IT WORKS HOLY FUCKING FUCKING FUCK");
+void henk() {
+    Serial.println("Henk hier");
 }
 
 void setup() {
     Serial.begin(9600);
-    handler.addURLMapping("/switch/group/on", thingCallback);
-    handler.addURLMapping("/otherthing", otherThingCallBack);
+    Serial.setTimeout(10000);
+    
+    handler.addURLMapping("/test/henk", henk);
 }
 
 
 void loop() {
     
-    handler.handle("/switch/group/on");
-    handler.handle("/switch/jemoeder/on");
-    handler.handle("/otherthing/henk");
-    handler.handle("/group/on");
-    handler.handle("/on");
-    Serial.println("And finally:");
-    handler.handle("/otherthing");
-    
-    delay(1000);
+    String str = Serial.readStringUntil('\n');
+    handler.handle(str.c_str());
 }
